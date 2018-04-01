26b7fa5e0a312f9e7fb612dd9bb4c980194347f9
Ged <gedkirkham@gmail.com>
2018-04-01 12:50:02 +0100

    Add project files.

---
+++ cleaning_rota/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cleaning_rota
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
