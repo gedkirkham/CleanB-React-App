db395738a5542ff381215d61ffeae9d51b851e62
Ged <gedkirkham@gmail.com>
2018-04-05 07:50:43 +0100

    Add unit tests

---
+++ cleaning_rota.test.unit/build_location_test.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Program;
+
+namespace cleaning_rota.test.unit
+{
+    [TestClass]
+    public class build_location_test
+    {
+        private readonly build_location _build_location = new build_location();
+
+        [TestMethod]
+        [Exception(Type(Exception))]//unsure how to properly do this
+        public void room_count_negative_test()
+        {
+            _build_location.room_count("*");
+            Assert.Equals(Exception);
+        }
+
+        [TestMethod]
+        public void number_in_number_out()
+        {
+            int result = _build_location.room_count("4");
+            Assert.Equals(4,result);
+        }
+
+        [TestMethod]
+        public void string_in_number_out()
+        {
+            int result = _build_location.room_count("Four");
+            Assert.Equals(4, result);
+        }
+
+        [TestMethod]
+        public void add_room()
+        {
+            String room_name = _build_location.add_room("Room1");
+            Assert.Equals("Room1", room_name);
+        }
+
+        [TestMethod]
+        public void true_room_frequency()
+        {
+            bool validate_room_frequency = _build_location.validate_room_frequency("2");
+            Assert.Equals(validate_room_frequency, true);
+        }
+
+        [TestMethod]
+        public void false_room_frequency()
+        {
+            bool validate_room_frequency = _build_location.validate_room_frequency("twoo");
+            Assert.Equals(validate_room_frequency, false);
+        }
+    }
+}
---
+++ cleaning_rota/build_location.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cleaning_rota
+{
+    class build_location
+    {
+        public int room_count(String _input)
+        {
+            int room_count_value = 0;
+            int converted_to_int;
+
+            if (Int32.TryParse(_input, out converted_to_int))
+            {
+                result = converted_to_int;
+            }
+            else
+            {
+                Exception //throw proper exceltion
+            }
+
+            private set {converted_to_int = room_count_value;}
+            return room_count_value;
+        }
+
+        public int get_room_count()
+        {
+            return this.room_count_value;
+        }
+    }
+}
