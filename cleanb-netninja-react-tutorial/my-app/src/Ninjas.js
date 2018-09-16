91a51881d9c0bae38cb53d8fcdab987110aefb71
Ged <gedkirkham@gmail.com>
2018-09-16 21:04:44 +0100

    Add Ninjas.js, and make associated amendments to App.js

---
+++ cleanb-netninja-react-tutorial/my-app/src/Ninjas.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Ninjas extends Component{
+    render(){
+        console.log(this.props);
+        const {name,age} = this.props;
+        return (
+            <div className="ninja">
+                <div>Name: {name}</div>
+                <div>Age: {age}</div>
+            </div>
+        )
+    }
+}
+
+export default Ninjas
