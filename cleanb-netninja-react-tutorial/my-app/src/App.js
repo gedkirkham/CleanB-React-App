0437f571defa4c3d30982ef51e2146d7875df711
Ged <gedkirkham@gmail.com>
2018-10-27 17:03:23 +0100

    Add <Switch> to website navigation to prevent posts being displayed alongside About and Contact pages. Create Post.js to allow individual posts to be displayed on their own independent page.

--- cleanb-netninja-react-tutorial/my-app/src/App.js
+++ cleanb-netninja-react-tutorial/my-app/src/App.js
@@ -4,7 +4,8 @@ import FooterNavBar from './Components/FooterNavBar';
 import Home from './Components/Home';
 import About from './Components/About';
 import Contact from './Components/Contact';
-import { BrowserRouter, Route, NavLink } from 'react-router-dom';
+import Post from './Components/Post';
+import { BrowserRouter, Route, NavLink, Switch } from 'react-router-dom';
 
 const App = () => {
     return (
@@ -12,9 +13,12 @@ const App = () => {
             <div className="cleanb-app container">
                 <LoginOrCreateAccount/>
                 <h1 className="blue-text container"><NavLink to="/">CleanB</NavLink></h1>
-                <Route exact path='/' component={Home} />
-                <Route path='/About' component={About} />
-                <Route path='/Contact' component={Contact} />
+                <Switch>
+                    <Route exact path='/' component={Home} />
+                    <Route path='/About' component={About} />
+                    <Route path='/Contact' component={Contact} />
+                    <Route path='/:post_id' component={Post} />
+                </Switch>
                 <FooterNavBar/>
             </div>
         </BrowserRouter>
--- cleanb-netninja-react-tutorial/my-app/src/Components/Home.js
+++ cleanb-netninja-react-tutorial/my-app/src/Components/Home.js
@@ -4,6 +4,7 @@ import Cleaners from '../Cleaners/Cleaners';
 import AddRoom from '../Rooms/AddRoom';
 import Rooms from '../Rooms/Rooms';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 class Home extends Component {
     state = {
@@ -117,7 +118,9 @@ class Home extends Component {
             return (
                 <div className="post card" key={post.id}>
                     <div className="card-content">
-                        <span className="card-title">{post.title}</span>
+                        <Link to={'/' + post.id}>
+                            <span className="card-title">{post.title}</span>
+                        </Link>
                         <p>{post.body}</p>
                     </div>
                 </div>
