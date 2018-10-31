2bf5d987152e70e2265bb15a2a886ed552d1ec76
Ged <gedkirkham@gmail.com>
2018-10-31 21:08:42 +0000

    Change redux if statements to switch.

--- my-app/src/Reducers/rootReducer.js
+++ my-app/src/Reducers/rootReducer.js
@@ -3,25 +3,29 @@ const initState = {
         {name: 'Ged', id: 1},
         {name: 'Tom', id: 2}
     ],
-    rooms: [ ]
+    rooms: [ 
+        {name: 'Room 1', id: 1},
+        {name: 'Room 2', id: 2}
+    ]
 }
 
 const rootReducer = (state = initState, action) => {
-    console.log(action);
-    if(action.type === 'DELETE_CLEANER') {
-        let newCleanerList = state.cleaners.filter(cleaners => {
-            return action.id !== cleaners.id
-        });
-        return {
-            ...state,
-            cleaners: newCleanerList
-        }
-    }
-    if(action.type === 'ADD_CLEANER') {
-        return {
-            ...state,
-            cleaners: [...state.cleaners, action.name]
-        }
+    switch(action.type){
+        case 'DELETE_CLEANER':
+            let newCleanerList = state.cleaners.filter(cleaners => {
+                return action.id !== cleaners.id
+            });
+            return {
+                ...state,
+                cleaners: newCleanerList
+            }
+        break;
+        case 'ADD_CLEANER':
+            return {
+                ...state,
+                cleaners: [...state.cleaners, action.name]
+            }
+        break;
     }
     return state;
 }
