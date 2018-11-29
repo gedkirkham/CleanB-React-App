3ac6b7d33fbb01f76e904ae4fd6f4013f02108ac
Ged Kirkham <ged.kirkham@ten10.com>
2018-11-29 18:02:36 +0000

    Create CreateCalendarDates const to handle first cleaning date generation.

--- my-app/src/Calendar/Calendar.js
+++ my-app/src/Calendar/Calendar.js
@@ -17,30 +17,28 @@ const CleanerRow = ({cleaners}) => {
     )
 }
 
-const CurrentDayOfTheWeek = () => {
-    var myDate = new Date();
-    var currentDayOfTheWeek = myDate.getDay();
+const DaysUntilSaturday = () => {
+    var currentDayOfTheWeekAsNum = new Date().getDay();
     var saturdayAsNum = 6;
-    if(currentDayOfTheWeek !== saturdayAsNum){
-        var DaysUntilSaturday = currentDayOfTheWeek - saturdayAsNum;
-        if(DaysUntilSaturday < 0) {
-            console.log("before *-1: "+currentDayOfTheWeek);
-            DaysUntilSaturday = DaysUntilSaturday * -1;
-            console.log("after *-1: "+currentDayOfTheWeek);
-        }
-        currentDayOfTheWeek = currentDayOfTheWeek + DaysUntilSaturday;
-        console.log("DaysUntilSaturday: "+DaysUntilSaturday);
-        console.log("currentDayOfTheWeek: "+currentDayOfTheWeek);
+    if(currentDayOfTheWeekAsNum !== saturdayAsNum){
+        var daysUntilSaturday = saturdayAsNum - currentDayOfTheWeekAsNum;
     }
     return (
-        DaysUntilSaturday
+        daysUntilSaturday
+    )
+}
+
+const CreateCalendarDates = () => {
+    var firstCleaningDate = new Date(new Date().getFullYear(),new Date().getMonth(),new Date().getDate() + DaysUntilSaturday()).toUTCString();
+    return (
+        firstCleaningDate
     )
 }
 
 const Calendar = ({cleaners, deleteCleaner, rooms, deleteRoom}) => {
     var rows = [1,2,3,4];
     var saturdayAsNum = 6;
-    var currentDayOfTheWeek = {CurrentDayOfTheWeek};//TODO: this value is returning 0 all the time
+    var currentDayOfTheWeek = DaysUntilSaturday();//TODO: this value is returning 0 all the time
     const CalendarDate = () => {
         if(currentDayOfTheWeek !== saturdayAsNum){
             var DaysUntilSaturday = currentDayOfTheWeek - saturdayAsNum;
@@ -76,9 +74,7 @@ const Calendar = ({cleaners, deleteCleaner, rooms, deleteRoom}) => {
                     })}
             </tbody>
             <tbody>
-                {<CalendarDate/>}
-                {<CurrentDayOfTheWeek/>}
-                {new Date().getDay()}
+                {<CreateCalendarDates/>}
             </tbody>
         </table>)
     return (
